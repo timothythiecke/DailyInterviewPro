59fc68f37ff71444292600207188fe62da071aef
Timothy Thiecke <timothy.thiecke@gmail.com>
2020-05-06 19:38:08 +0200

    Potential solution for 20200506

--- 20200506/main.py
+++ 20200506/main.py
@@ -1,6 +1,22 @@
 def singleNumber(nums):
   # Fill this in.
-  return -1
+
+    m = {}
+
+    for digit in nums:  
+        m[digit] = m.get(digit, 0) + 1 # Auto fill 0 if not exists
+
+    for key in m.keys():
+        if m[key] == 1:
+            return key
+
+    return -1
 
 print (singleNumber([4, 3, 2, 4, 1, 3, 2]))
 # 1
+
+
+# Print unique number inside array
+# Best idea is to use a map/dictionary, and count the amount of times an element occurs, then look for the element with only 1
+
+# Challenge: use only O(1) memory
