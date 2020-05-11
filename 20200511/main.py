273f0cc3d16353e2edcbf0998cfbdcc1c836f055
Timothy Thiecke <timothy.thiecke@gmail.com>
2020-05-11 21:00:31 +0200

    Potential solution

--- 20200511/main.py
+++ 20200511/main.py
@@ -2,6 +2,8 @@
 
 def staircase(n):
   # Fill this in.
+
+  
   
 print(staircase(4))
 # 5
@@ -12,3 +14,8 @@ print(staircase(5))
 
 
 # The first thing that springs to mind with the solutions 5 and 8 suggests that it may have something to do with the fibonacci series
+# Writing out and testing the different possible cases on a sheet of paper shows that this is indeed the case
+# 1 -> 2 -> 3 -> 4 -> 5 -> 6  -> 7
+# 1 -> 2 -> 3 -> 5 -> 8 -> 13 -> 21
+# Therefore we simply need to implement Fibonacci to solve this problem
+# In order to achieve a solution in O(n) time, we can implement Fibonacci iteratively
